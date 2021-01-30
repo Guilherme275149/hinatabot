1d321acb3f083ed1b0605db98cacae4e20709fd5
Guilherme275149 <78052265+Guilherme275149@users.noreply.github.com>
2021-01-30 13:28:15 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,33 @@
+const menuadmin = (prefix, pushname) => {
+ return `Slv aqui e o menu dos admins
+ _obs para o bot executar esses comandos e nescessario dar adm para o bot_
+ 
+ ◪🔱 *COMANDO DOS ADMINS* 🔱
+ │
+ ├─ ▫️ ${prefix}opengc
+ ├─ ▫️ ${prefix}closegc
+ ├─ ▫️ ${prefix}promote
+ ├─ ▫️ ${prefix}demote
+ ├─ ▫️ ${prefix}tagall
+ ├─ ▫️ ${prefix}tagall2
+ ├─ ▫️ ${prefix}tagall3
+ ├─ ▫️ ${prefix}tagall4
+ ├─ ▫️ ${prefix}tagall5
+ ├─ ▫️ ${prefix}add
+ ├─ ▫️ ${prefix}kick
+ ├─ ▫️ ${prefix}listadmins
+ ├─ ▫️ ${prefix}linkgroup
+ ├─ ▫️ ${prefix}leave
+ ├─ ▫️ ${prefix}welcome
+ ├─ ▫️ ${prefix}nsfw
+ ├─ ▫️ ${prefix}leveling
+ ├─ ▫️ ${prefix}level
+ ├─ ▫️ ${prefix}delete
+ ├─ ▫️ ${prefix}simih
+ └─ ▫️ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
